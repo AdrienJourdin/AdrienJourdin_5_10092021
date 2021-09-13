9b885651dc83fd6becd204cc0deac21812848a5c
Adrien Jourdin <adrjourdin@gmail.com>
2021-09-13 17:06:51 +0200

    ajout de la version améliorée de la classe panier (ajout de produit, retrait et vider le panier)

--- js/page_produit.js
+++ js/page_produit.js
@@ -1,104 +1,139 @@
 var idProduct = localStorage.getItem('idProduct');
 
 const chargerProduct = () => {
-    fetch("http://localhost:3000/api/cameras/" + idProduct.toString())
-        .then((res) => {
-            console.log("http://localhost:3000/api/cameras/" + idProduct.toString());
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then((value) => {
-            const cam = value;
-            console.log(cam);
-            let objetCam = new Camera(cam.lenses, cam._id, cam.name, cam.price, cam.description, cam.imageUrl);
-            createCamHTML(objetCam, "camera", "produit");
+  fetch("http://localhost:3000/api/cameras/" + idProduct.toString())
+    .then((res) => {
+      console.log("http://localhost:3000/api/cameras/" + idProduct.toString());
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((value) => {
+      const cam = value;
+      console.log(cam);
+      let objetCam = new Camera(cam.lenses, cam._id, cam.name, cam.price, cam.description, cam.imageUrl);
+      createCamHTML(objetCam, "camera", "produit");
+      localStorage.setItem("objectSelected", JSON.stringify(objetCam));
 
+    })
+    .catch((err) => {
+      alert(err);
+    })
+    .finally(() => {
+    }
 
-        })
-        .catch((err) => {
-            alert(err);
-        })
-        .finally(() => {
-        }
-
-        );
+    );
 }
 
 //crée le bloc camera 
 createCamHTML = (objet, className, sectionName) => {
 
-    let sectionCam = document.createElement("section");
-    let body= document.querySelector("body");
-    sectionCam.classList.add(sectionName);
-    body.appendChild(sectionCam);
-    const divCam = document.createElement("div");
-    divCam.classList.add(className);
-    sectionCam.appendChild(divCam);
-    let arrayProperty = Object.getOwnPropertyNames(objet);
-    for (propertyName of arrayProperty) {
-      createElementCameraHTML(objet, className, propertyName);
-    }
-  
-  }
-  
-  //Fonction pour créer un élément d'un bloc, cette fonction sera ensuite contenue dans la fonction createCameraHTML(qui elle crée le bloc camera)
-  createElementCameraHTML = (objet, className, param) => {
-    let divCam = document.querySelector("." + className);
-    let divElem = document.createElement("div");
-    divElem.classList.add(className + "__" + param);
-    divCam.appendChild(divElem);
-    if (param != "imageUrl") {
-      divElem.innerText = objet[param];
-    }
-    else {
-      let Image = document.createElement("img");
-      divElem.appendChild(Image);
-      fetch(objet[param])
-        .then((res) => {
-          if (res.ok) {
-            return res.blob();
-          }
-        }).then((value) => {
-          const imageUrl = URL.createObjectURL(value);
-          Image.setAttribute("width", "270");
-          Image.setAttribute("height", "270");
-          Image.src = imageUrl;
-        })
-    }
-  
+  let sectionCam = document.createElement("section");
+  let body = document.querySelector("body");
+  sectionCam.classList.add(sectionName);
+  body.appendChild(sectionCam);
+  const divCam = document.createElement("div");
+  divCam.classList.add(className);
+  sectionCam.appendChild(divCam);
+  let arrayProperty = Object.getOwnPropertyNames(objet);
+  for (propertyName of arrayProperty) {
+    createElementCameraHTML(objet, className, propertyName);
   }
 
-  //appel de la fonction pour charger le produit sélectionné précédemment
+}
+
+//Fonction pour créer un élément d'un bloc, cette fonction sera ensuite contenue dans la fonction createCameraHTML(qui elle crée le bloc camera)
+createElementCameraHTML = (objet, className, param) => {
+  let divCam = document.querySelector("." + className);
+  let divElem = document.createElement("div");
+  divElem.classList.add(className + "__" + param);
+  divCam.appendChild(divElem);
+  if (param != "imageUrl") {
+    divElem.innerText = objet[param];
+  }
+  else {
+    let Image = document.createElement("img");
+    divElem.appendChild(Image);
+    fetch(objet[param])
+      .then((res) => {
+        if (res.ok) {
+          return res.blob();
+        }
+      }).then((value) => {
+        const imageUrl = URL.createObjectURL(value);
+        Image.setAttribute("width", "270");
+        Image.setAttribute("height", "270");
+        Image.src = imageUrl;
+      })
+  }
+
+}
+
+//appel de la fonction pour charger le produit sélectionné précédemment
 chargerProduct();
 
 
-
-
-
 //Creation de la div panier
-createPanier=()=>{
-    let panier=[];
-    let body=document.querySelector("body");
-    let ajoutPanier=document.createElement('a');
-    body.classList.add("Ajout_Panier");
-    ajoutPanier.innerText="Ajout au panier";
-    body.appendChild(ajoutPanier);
-    ajoutPanier.addEventListener('click',(e)=>{
-        e.preventDefault();
-        e.stopPropagation();
-        let pan=localStorage.getItem('panier');
-        if(pan.includes("idProduct")){
-            alert("ce produit est deja dans votre panier");
-        }else{
-            panier=localStorage.getItem('panier');
-            console.log(panier);
-            panier.push(idProduct);
-            localStorage.setItem('panier', panier);
-        }
-    })
+createAjoutAuPanier = () => {
+  let body = document.querySelector("body");
+  let ajoutPanier = document.createElement('a');
+  body.classList.add("Ajout_Panier");
+  ajoutPanier.innerText = "Ajout au panier";
+  body.appendChild(ajoutPanier);
+  ajoutPanier.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let panier = JSON.parse(localStorage.getItem('panier'));
+    if (panier == null) {
+      panier = new Panier([], [], []);
+    }
+    let camToAdd = JSON.parse(localStorage.getItem('objectSelected'));
+    let panier2 = new Panier(panier.listeId, panier.listePrice, panier.numberOfProduct);
+    panier2.addToCart(camToAdd);
+    console.log("panier2", panier2);
+    localStorage.setItem('panier', JSON.stringify(panier2));
+  }
+  )
 }
 
+createRetirerDuPanier = () => {
+  let body = document.querySelector("body");
+  let retraitPanier = document.createElement('a');
+  body.classList.add("Retrait_Panier");
+  retraitPanier.innerText = "Retirer du panier";
+  body.appendChild(retraitPanier);
+  retraitPanier.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let panier = JSON.parse(localStorage.getItem('panier'));
+    if (panier == null) {
+      panier = new Panier([], [], []);
+    }
+    let camToRemove = JSON.parse(localStorage.getItem('objectSelected'));
+    let panier2 = new Panier(panier.listeId, panier.listePrice, panier.numberOfProduct);
+    console.log("panier2", panier2);
+    panier2.RemoveToCart(camToRemove);
 
+    localStorage.setItem('panier', JSON.stringify(panier2));
+  }
+  )
+}
 
-createPanier();
+createViderLePanier = () => {
+  let body = document.querySelector("body");
+  let viderPanier = document.createElement('a');
+  body.classList.add("Vider_Panier");
+  viderPanier.innerText = "Vider le panier";
+  body.appendChild(viderPanier);
+  viderPanier.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    localStorage.removeItem('panier');
+    panier2 = new Panier([], [], []);
+  }
+  )
+}
+
+createAjoutAuPanier();
+createRetirerDuPanier();
+createViderLePanier();
--- js/panier.js
+++ js/panier.js
@@ -1,25 +1,49 @@
-class Panier{
-    constructor(){
-        this.listeId=[];
-        this.listePrice=[];
-        this.numberOfProduct=[];
+class Panier {
+    constructor(listeId, listePrice,numberOfProduct) {
+        this.listeId = listeId;
+        this.listePrice = listePrice;
+        this.numberOfProduct = numberOfProduct;
     }
 
-    addToCart=(camera)=>{
-        this.listeId.push(camera._id);
-        this.listePrice.push(camera.price);
-        console.log("listeId",this.listeId);
+    addToCart = (camera) => {
+        //Si le produit est deja dans le panier on augmente sa quantité de 1
+        if (this.listeId.includes(camera._id)) {
+
+            let indexCam = this.listeId.indexOf(camera._id);
+            this.numberOfProduct[indexCam]=this.numberOfProduct[indexCam]+1;
+        }
+        //Si le produit n'y est pas on l'ajoute au panier et on fixe sa quantité à 1
+        else {
+            this.listeId.push(camera._id);
+            this.listePrice.push(camera.price);
+            this.numberOfProduct.push(1);
+        }
     }
-    
-    removeToCart=(camera)=>{
-        let indexToRemove=this.listeId.indexOf(camera._id);
-        this.listeId.splice(indexToRemove,indexToRemove-1);
-        this.listePrice.splice(indexToRemove,indexToRemove-1);
+
+    RemoveToCart = (camera) => {
+        
+        let indexToRemove = this.listeId.indexOf(camera._id);
+        if (this.listeId.includes(camera._id)) {
+            //si le nombre de produit est supérieur à 1 alors on retire une quantité au produit
+            if (this.numberOfProduct[indexToRemove] > 1) {
+                this.numberOfProduct[indexToRemove]-=1;
+            }
+            //Si le nombre de produit est inférieur ou égale à 1 on retire le produit du panier totalement
+            else {
+                console.log('bonjour');
+                this.listeId.splice(indexToRemove, 1 );
+                this.listePrice.splice(indexToRemove, 1 );
+                this.numberOfProduct.splice(indexToRemove, 1 );
+            }
+        }
+        else{
+            alert("aucun produit de cette référence ne se trouve dans le panier")
+        }
     }
-    calculatePrice=()=>{
-        let sum=0;
-        for(let price of this.listePrice){
-            sum+=price;
+    calculatePrice = () => {
+        let sum = 0;
+        for (let price of this.listePrice) {
+            sum += price;
         }
         return sum;
     }
